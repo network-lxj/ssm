920632fd79336bad0ead37b8b1f60652a671d675
lxj <1736484489@qq.com>
2021-05-24 14:34:01 +0800

    构建项目

---
+++ src/test/java/com/example/ssm_demo/SsmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssm_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsmDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
